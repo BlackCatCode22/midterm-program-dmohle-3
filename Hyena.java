3df86724ffc9e225ac7423cd031534d7a279d3aa
dmohle <10038913+dmohle@users.noreply.github.com>
2023-09-21 12:24:18 -0700

    Add files via upload

---
+++ Hyena.java
@@ -0,0 +1,13 @@
+public class Hyena extends Animal {
+    public static int numOfHyenas = 0;
+
+    // Let's create a constructor
+    public Hyena () {
+        numOfHyenas++;
+    }
+
+
+
+   String HyenaHouse;
+
+}
