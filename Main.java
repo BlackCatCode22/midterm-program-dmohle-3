19c8f246a8c66a04690e8008ab3de834989e26bb
dmohle <10038913+dmohle@users.noreply.github.com>
2023-09-21 12:24:43 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,38 +1,148 @@
+// dH 9/19/23
+// Main.java
+// Driver class for zooWarmUp
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class Main {
+    // Create the genUniqueIDFunction
+
+    // values for theSpecies:
+    //   lion
+    String genUniqueID(String theSpecies ) {
+        if (theSpecies.equals("lion")) {
+            // do something
+        }
+        return "the name";
+    }
+
+
     public static void main(String[] args) {
 
-        System.out.println("Welcome to my Cat Program!");
+        System.out.println("Welcome to my Zoo Program!");
 
-        // Create a cat object using the constructor with no arguments
-        Cat myCat = new Cat();
+        // How many animals do we have.
+        System.out.println("\n Before we use 'new' ... we have " + Animal.numOfAnimals + " animals!");
 
-        // Assign values to the cat object fields.
-        myCat.age = 3;
-       // myCat.name = "Felix";
-        myCat.catSound = "GGGGG";
-        // myCat.livesRemaining
+        // Create an animal object.
+        Animal myNewAnimal = new Animal();
 
-        System.out.println(" My new cat's name is: " + myCat.name);
-        myCat.meow();
+        myNewAnimal.name = "Leo";
 
-        // Create an ArrayList of five cats
-        ArrayList<Cat> catList = new ArrayList<>();
+        System.out.println("\n the name of the new animal is: " + myNewAnimal.name);
 
-        // Use a for loop to create five Cat objects and add them to the ArrayList
-        for (int i = 1; i <= 5; i++) {
-            // using the constructor with arguments   Cat1, Cat2, etc.,
-            Cat cat = new Cat("Cat" + i);
-            catList.add(cat);
+        // Create a hyena.
+        Hyena myHyena = new Hyena();
+
+        Hyena myHyena02 = new Hyena();
+        Hyena myHyena03 = new Hyena();
+
+        // How many hyenas do we have?
+        System.out.println("We have " + Hyena.numOfHyenas + " hyenas!");
+
+
+        // Give the new hyena a name...
+        myHyena.name = "Zig";
+
+        System.out.println("\n My new hyena is named: " + myHyena.name);
+
+        myHyena.makeNoise();
+
+        // How many animals do we have now.
+        System.out.println("\n After using new twice we have " + Animal.numOfAnimals + " animals!");
+
+
+
+        System.out.println("He makes this noise: " + myHyena.makeNoise02());
+
+
+
+        // open the arriving animal text file
+        // Define the file path
+
+
+     /*
+        String filePath = "C:\\Users\\BE218\\IdeaProjects\\zooWarmUp\\files\\arrivingAnimals.txt";
+
+        String theSpecies = "";
+
+
+
+        // Create an ArrayList to hold the animal objects!
+        ArrayList<Animal> animalList = new ArrayList<>();
+
+        try {
+            // Create a FileReader to read the file
+            FileReader fileReader = new FileReader(filePath);
+
+            // Create a BufferedReader to efficiently read lines
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+
+
+
+            // Read and print each line from the file
+            while ((line = bufferedReader.readLine()) != null) {
+                // System.out.println(line);
+                // Split the line into 6 substring
+                String[] myArray = line.split(",");
+
+
+                // Create an Animal object from the Animal class.
+                Animal anyOldAnimal = new Animal();
+
+                // Increment the static Animal count
+                Animal.numOfAnimals++;
+
+
+                // fill the object's data fields
+                anyOldAnimal.desc = myArray[0];
+                anyOldAnimal.birthSeason = myArray[1];
+                anyOldAnimal.color = myArray[2];
+                anyOldAnimal.weight = myArray[3];
+                anyOldAnimal.origin01 = myArray[4];
+                anyOldAnimal.origin02 = myArray[5];
+
+                // Add the newly created animal object to the array list
+                animalList.add(anyOldAnimal);
+
+            }
+
+            // Close the BufferedReader and FileReader
+            bufferedReader.close();
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("An error occurred while reading the file.");
         }
 
+        // We are done with the text file.
+        // Output the ArrayList!
         // Output the catList with an item in loop
-        for (Cat cat : catList) {
-            System.out.println(cat.name + " " + cat.age);
+        for (Animal animal : animalList) {
+            System.out.println(animal.desc);
+            // Get the species
+
+            System.out.println(animal.birthSeason);
+            System.out.println(animal.color);
+            System.out.println(animal.weight);
+            System.out.println(animal.origin01);
+            System.out.println(animal.origin02);
+            System.out.println("\n\n");
         }
+
+        System.out.println(" The number of animals is: " + Animal.numOfAnimals);
+*/
 
 
     }
-
 }
+
+
+
+
